45e68e0e13c8c3237e4636b2f1e7a94e9e1f4294
Albin Matson Gyllang <albingyllang@gmail.com>
2022-06-17 11:24:54 +0200

    Setting up
    - Setting up project w/ "npm init"
    - Basic html template
    - Simple styling

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ interface Video {
 
 // GET http://localhost:3000/videos
 const getVideos = (): Promise<Video[]> => {
+
 }
 
 // PUT http://localhost:3000/videos/${id}
-const updateVideo = async (id, changes): Promise<Video> => {};
+const updateVideo = async (id, changes): Promise<Video> => {
+  
+};
